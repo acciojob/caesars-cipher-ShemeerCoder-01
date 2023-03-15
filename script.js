a0440e8a6f608cd7fadaeb363265108b5cf5fdab
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-03-15 12:11:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Your Script here.
+var str = "";
+function ROT13(value) {
+	var size = value.length;
+	for (var i = 0; i < size; i++) {
+			if(value.charAt(i) >= 'A' && value.charAt(i) <= 'Z'){
+			var dis = value.charAt(i) - 'A';
+			var char =(char)( (dis + 13) % 26 ) + value.charAt(i);
+			str += char;
+		}
+		
+	}
+	return str;
+	
+}
